3b1114d159d20ac9411b40715c52fbea145ed60b
mobcoder-manish-khulbe <mobcoder-manish-khulbe>
2023-04-11 10:46:39 +0530

    written skaffold yaml file

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
         <div>
-      <h1 className='text-3xl font-bold underline' >CreatePost</h1>
+      <h1 className='text-3xl font-bold underline' >CreatePost !!</h1>
         <PostCreate />
         <PostList/>
         </div>
